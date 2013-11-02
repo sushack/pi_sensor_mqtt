c3f577055668ce4177a6f5cde99a46a09d6336a8
James Smith <james@floppy.org.uk>
2013-11-02 17:37:06 +0000

    Slow down publishing to every 10 seconds
    And reduce serial latency

--- sensor_mqtt.py
+++ sensor_mqtt.py
@@ -3,10 +3,13 @@
 import mosquitto
 import serial
 import os
-from datetime import datetime, date, time
+import time
+from datetime import datetime, date
 import json
 import random
 import yaml
+
+last_published = 0
 
 # Load config
 stream = open("config.yml", 'r')
@@ -63,18 +66,24 @@ def publish(sensor, reading_type, reading):
                     "lon": sensor_config['longitude']
                 }
             }
+            print(json.dumps(data))
             mqttc.publish(mqtt_topic(sensor, reading_type), json.dumps(data))
             print("message published: " + sensor + " " + reading_type)
     
 while mqttc.loop() == 0:
+    serialFromWireless.flushInput()
+    serialFromWireless.flushOutput()
+    serialFromWireless.flush()
     if serialFromWireless.read() == b'a': #look for start of data package
         rawinput = serialFromWireless.read(11).decode("utf-8") #'a' plus rest of message = 12 bytes
-        #chop up raw data input
+        # chop up raw data input
         sensor = rawinput[0:2]
         river = rawinput[2:6]
         number = rawinput[6:]
         number = number.rsplit('-')[0] #remove trailing dashes
-        publish(sensor,river,float(number))
+        if last_published < time.time() - 10:
+            publish(sensor,river,float(number))
+            last_published = time.time()
     pass
 
 def cleanup():
